2cb0d6c07e9a1d1a493d5bd1af6949d0b3990a2d
Nikushaana <nikanashvili@gmail.com>
2025-08-08 19:22:57 +0400

    updated

--- domains-nest/src/main.ts
+++ domains-nest/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://domains-market.vercel.app'], // allow your frontend URLs
+    origin: ['http://localhost:3000', 'https://domains-market-nest.vercel.app'], // allow your frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
